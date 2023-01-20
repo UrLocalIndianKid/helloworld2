0377a95bfc63b7957eb287c7e10a954ba5a8770c
UrLocalIndianKid <122555211+UrLocalIndianKid@users.noreply.github.com>
2023-01-20 12:25:12 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,21 +9,12 @@ input.onButtonPressed(Button.A, function () {
     music.playMelody("C D E F G A B C5 ", 226)
 })
 input.onButtonPressed(Button.AB, function () {
-    basic.showString("SHAKE ME")
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showLeds(`
-        . . # . .
-        . # . . .
-        # # # # #
-        . # . . .
-        . . # . .
-        `)
-    music.playMelody("C5 B A G F E D C ", 226)
-})
-input.onGesture(Gesture.Shake, function () {
 	
 })
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    music.startMelody(music.builtInMelody(Melodies.Wawawawaa), MelodyOptions.Once)
+input.onGesture(Gesture.Shake, function () {
+    music.ringTone(10000)
+    music.setVolume(25599)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
 })
+music.setVolume(255)
+music.playMelody("C D E F G A B C5 ", 2554)
